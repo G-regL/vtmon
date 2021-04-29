43b6d63241c916f64dba7b66fecd2de0241ee3db
Greg Linton <Greg.Linton@canada.ca>
2021-04-29 11:55:28 -0400

    switched from Swarm configs to volume-mapped files

--- bin/pre-setup2.sh
+++ bin/pre-setup2.sh
@@ -19,9 +19,6 @@ function pull_docker_images () {
   done
 }
  
-
-
-
 function make_persistent_storage () {
   dir=$1
   perms=$2
@@ -71,6 +68,14 @@ function make_filesystem () {
   echo "${CHECK} Mount filesystem"
 }
 
+function copy_service_config () {
+  f=`basename $1`
+
+  echo "${SPACE} $f"
+  cp $1 $2
+  echo "${CHECK} $f"
+}
+
 
 echo " Install/remove packages"
 # -------- Packages
@@ -100,22 +105,62 @@ echo
 
 
 echo " Make persistent storage"
-#Portainer
-make_persistent_storage /opt/docker/stack.Portainer/service.portainer
+# Portainer
+echo " Portainer"
+make_persistent_storage /opt/docker/Portainer/portainer/data/
 # GraphHouse
-make_persistent_storage /opt/docker/stack.GraphHouse/service.clickhouse/data/
-make_persistent_storage /opt/docker/stack.GraphHouse/service.clickhouse/metadata
-make_persistent_storage /opt/docker/stack.GraphHouse/service.carbon
-#Grafana
-make_persistent_storage /opt/docker/stack.grafana/service.grafana/data/ 472:472
+echo " GraphHouse"
+make_persistent_storage /opt/docker/GraphHouse/api/config/
+make_persistent_storage /opt/docker/GraphHouse/carbon/data/
+make_persistent_storage /opt/docker/GraphHouse/carbon/config/
+make_persistent_storage /opt/docker/GraphHouse/clickhouse/data/
+make_persistent_storage /opt/docker/GraphHouse/clickhouse/metadata/
+make_persistent_storage /opt/docker/GraphHouse/clickhouse/config/
+make_persistent_storage /opt/docker/GraphHouse/graphite/config/
+make_persistent_storage /opt/docker/GraphHouse/relay/config/
+# Grafana
+echo " Grafana"
+make_persistent_storage /opt/docker/Grafana/grafana/data/ 472:472
+
+# Telegraf
+echo " Telegraf"
+make_persistent_storage /opt/docker/Telegraf/cluster/
+make_persistent_storage /opt/docker/Telegraf/datacenter/
+make_persistent_storage /opt/docker/Telegraf/datastore/
+make_persistent_storage /opt/docker/Telegraf/host/
+make_persistent_storage /opt/docker/Telegraf/vm/
+
+
 echo
 
+echo " Copy configs"
+# GrapHouse
+echo " GraphHouse"
+copy_service_config res/swarm/config/Graphhouse/api/api.yaml /opt/docker/GraphHouse/api/config/
+copy_service_config res/swarm/config/Graphhouse/carbon/carbon-clickhouse.conf /opt/docker/GraphHouse/carbon/config/
+copy_service_config res/swarm/config/Graphhouse/clickhouse/rollup.xml /opt/docker/GraphHouse/clickhouse/config/
+copy_service_config res/swarm/config/Graphhouse/clickhouse/init.sql /opt/docker/GraphHouse/clickhouse/config/
+copy_service_config res/swarm/config/Graphhouse/graphite/graphite-clickhouse.conf /opt/docker/GraphHouse/graphite/config/
+copy_service_config res/swarm/config/Graphhouse/relay/relay.yaml /opt/docker/GraphHouse/relay/config/
+# Telegraf
+echo " Telegraf"
+copy_service_config res/swarm/config/Telegraf/cluster/telegraf.conf /opt/docker/Telegraf/cluster/
+copy_service_config res/swarm/config/Telegraf/datacenter/telegraf.conf /opt/docker/Telegraf/datacenter/
+copy_service_config res/swarm/config/Telegraf/datastore/telegraf.conf /opt/docker/Telegraf/datastore/
+copy_service_config res/swarm/config/Telegraf/host/telegraf.conf /opt/docker/Telegraf/host/
+copy_service_config res/swarm/config/Telegraf/vm/telegraf.conf /opt/docker/Telegraf/vm/
+
+ 
 echo " Pull Docker images"
-#Portainer
+# Portainer
+echo " Portainer"
 pull_docker_images $(cat res/swarm/stacks/portainer.yml | grep image | awk -F\  '{print $2}' | uniq)
 # GraphHouse
+echo " GraphHouse"
 pull_docker_images $(cat res/swarm/stacks/graphhouse.yml | grep image | awk -F\  '{print $2}' | uniq)
 # Grafana
+echo " Grafana"
 pull_docker_images $(cat res/swarm/stacks/grafana.yml |grep image |awk -F\  '{print $2}' |uniq)
 # Telegraf
+echo " Telegraf"
 pull_docker_images $(cat res/swarm/stacks/telegraf.yml |grep image |awk -F\  '{print $2}' |uniq)
